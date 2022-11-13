73ac05036b2d1357a33625ad8b74e70e3e43efbd
Kurt Obando <kurtobando.developer@gmail.com>
2022-11-13 12:23:19 +0800

    correct fn

--- Home Assignment. Recursion and Array methods/task-02.js
+++ Home Assignment. Recursion and Array methods/task-02.js
@@ -1,8 +1,11 @@
 let InvoiceList = ['Invoice 008', 'Invoice 007', 'Invoice 006', 'Invoice 005'];
 
 // insertToBottom, which adds invoices at the bottom of the lis
-function insertToBottom(invoice) {
-    InvoiceList.push(invoice);
+function insertToBottom(invoiceCount) {
+    if (invoiceCount > 0) {
+        InvoiceList.push(`Invoice 00${invoiceCount}`);
+        insertToBottom(invoiceCount - 1);
+    }
 }
 
 // reverseStack, which reverses the order of the invoices
@@ -10,11 +13,8 @@ function reverseList() {
     InvoiceList.reverse();
 }
 
-insertToBottom('Invoice 004');
-insertToBottom('Invoice 003');
-insertToBottom('Invoice 002');
-insertToBottom('Invoice 001');
 console.log(InvoiceList.join(' '));
-
+insertToBottom(4);
+console.log(InvoiceList.join(' '));
 reverseList();
 console.log(InvoiceList.join(' '));
